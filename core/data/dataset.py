ce7d9a0ce50e375f71e943c545ad2ca687cee369
Jessie Zhong <jz@jessiezhong.com>
2025-09-14 21:44:03 -0700

    fix

--- core/data/dataset.py
+++ core/data/dataset.py
@@ -117,10 +117,13 @@ def create_dataloaders(
     val_transform = BeatmapTransform(normalizer, augment=False)
     val_dataset = BeatmapDataset(val_data, val_transform)
 
+    # Use the actual vector dimension from the data, falling back to config if needed
+    actual_vector_dim = train_data[0][0].shape[1] if train_data else config['data']['in_channels']
+    
     collate_with_args = lambda batch: collate_fn(
         batch,
         max_seq_len=config['data']['max_seq_len'],
-        vector_dim=config['data']['in_channels'],
+        vector_dim=actual_vector_dim,
         device=device
     )
 
