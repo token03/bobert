9787a463dbb01c2e4ef71a46accfcb79c699b5e1
Jessie Zhong <jz@jessiezhong.com>
2025-12-30 23:55:42 -0800

    umapyoi umapyoi

--- scripts/fetch_beatmapsets.py
+++ scripts/fetch_beatmapsets.py
@@ -1,11 +1,9 @@
-
 import os
 import requests
 from dotenv import load_dotenv
 from rich import print
 
 def get_access_token(client_id: int, client_secret: str) -> str:
-    """Performs Client Credentials Grant to get an OAuth2 access token."""
     url = "https://osu.ppy.sh/oauth/token"
     data = {
         "client_id": client_id,
@@ -23,8 +21,19 @@ def get_access_token(client_id: int, client_secret: str) -> str:
     return response.json().get("access_token")
 
 def get_beatmapset(beatmapset_id: int, token: str):
-    """Calls the beatmapset endpoint using the Bearer token."""
     url = f"https://osu.ppy.sh/api/v2/beatmapsets/{beatmapset_id}"
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+    
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+def get_tag_map(token: str):
+    url = f"https://osu.ppy.sh/api/v2/tags"
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
@@ -49,4 +58,15 @@ token = get_access_token(int(client_id), client_secret)
 
 # 2. Get the beatmapset data
 beatmapset_data = get_beatmapset(2252729, token)
-print(beatmapset_data)
+
+tags = get_tag_map(token)
+tag_map = {tag['id']: tag['name'] for tag in tags['tags']}
+
+for beatmap in beatmapset_data['beatmaps']:
+    print(beatmap['version'], beatmap['id'])
+    for tag in beatmap['top_tag_ids']:
+        id = tag['tag_id']
+        count = tag['count']
+        print(f" - {tag_map[id]}: {count}")
+print(beatmapset_data['genre']['id'], beatmapset_data['genre']['name'])
+print(beatmapset_data['language']['id'], beatmapset_data['language']['name'])
